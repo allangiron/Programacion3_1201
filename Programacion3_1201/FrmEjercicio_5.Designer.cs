c608b7099e6ad8734eb64f5f0b8aff05b7d2f898
Allan Giron <86334962+08011498@users.noreply.github.com>
2021-07-01 14:58:42 -0600

    Primer Commit
    Commit Ejercicio # 5 seccion 1201 programacion 3

---
+++ Programacion3_1201/FrmEjercicio_5.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Programacion3_1201
+{
+    partial class FrmEjercicio_5
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_ejecutar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generar Matriz ";
+            // 
+            // btn_ejecutar
+            // 
+            this.btn_ejecutar.Location = new System.Drawing.Point(105, 55);
+            this.btn_ejecutar.Name = "btn_ejecutar";
+            this.btn_ejecutar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ejecutar.TabIndex = 1;
+            this.btn_ejecutar.Text = "Ejecutar";
+            this.btn_ejecutar.UseVisualStyleBackColor = true;
+            this.btn_ejecutar.Click += new System.EventHandler(this.btn_ejecutar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(60, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(154, 119);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(60, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // FrmEjercicio_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 218);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_ejecutar);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmEjercicio_5";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ejecutar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Programacion3_1201/FrmEjercicio_5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion3_1201
+{
+    public partial class FrmEjercicio_5 : Form
+    {
+        public FrmEjercicio_5()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ejecutar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[2, 2];
+
+            matriz[0, 0] = Convert.ToInt32(textBox1.Text);
+            matriz[0, 1] = Convert.ToInt32(textBox2.Text);
+            matriz[1, 0] = Convert.ToInt32(textBox3.Text);
+            matriz[1, 1] = Convert.ToInt32(textBox4.Text);
+
+            for (int filas = 0 ; filas < matriz.GetLength(0); filas++)
+            {
+                for(int colum = 0; colum < matriz.GetLength(1); colum++)
+                {
+                    MessageBox.Show("La posición es: " + filas + " , " + colum + " = " + matriz[filas, colum].ToString());
+                }
+            }
+
+        }
+    }
+}
